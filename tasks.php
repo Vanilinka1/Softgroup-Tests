9591defa6fca231af72835c573fb41ec60282bcf
MyDark <agressortm@gmail.com>
2017-01-03 08:37:39 +0200

    Just starting

---
+++ tasks.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Softgroups Junior PHP developer courses entrance test">
+    <meta name="author" content="Artur Lukashchuk">
+    <title>Junior PHP developer courses entrance test</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="assets/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <img src="assets/img/logo-1.png" alt="Softgroup logo" width="158" height="54">
+            <a class="navbar-brand" href="/"></a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li>
+                    <a href="/">Главная</a>
+                </li>
+                <li>
+                    <a href="/about.php">Автор</a>
+                </li>
+                <li>
+                    <a href="//softgroup.ua" target="_blank">Soft Group</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /.navbar-collapse -->
+    </div>
+    <!-- /.container -->
+</nav>
+
+<!-- Page Content -->
+<div class="container">
+    <div class="row">
+
+
+        <div class="col-md-9">
+
+            <div class="thumbnail">
+                <img class="img-responsive" src="http://placehold.it/800x300" alt="">
+                <div class="caption-full">
+                    <h4 class="pull-right">$24.99</h4>
+                    <h4>Обо мне:</h4>
+                    <p>See more snippets like these online store reviews at <a target="_blank"
+                                                                               href="http://bootsnipp.com">Bootsnipp -
+                            http://bootsnipp.com</a>.</p>
+                    <p>Want to make these reviews work? Check out
+                        <strong><a
+                                href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">this
+                                building a review system tutorial</a>
+                        </strong>over at maxoffsky.com!</p>
+                    <p>
+                        <form action="/index.php" method="post">
+
+                    <p>Имя: <input name="name" type="text"></p>
+
+                    <p>Фамилия: <input name="surname" type="text"></p>
+
+                    <p>E-mail: <input name="email" type="text"></p>
+
+                    <p>Сообщение: <br/><textarea name="message" cols="30" rows="5"></textarea></p>
+
+                    <p><input type='submit' value='Отправить'></p>
+
+                    </form>
+                    </p>
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+<!-- /.container -->
+
+<div class="container">
+    <hr>
+    <!-- Footer -->
+    <footer>
+        <div class="row">
+            <div class="col-lg-12">
+                <p>Copyright &copy; Artur Lukashchuk 2016</p>
+            </div>
+        </div>
+    </footer>
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<!-- Bootstrap Core JavaScript -->
+<script src="assets/js/bootstrap.min.js"></script>
+</body>
+</html>
